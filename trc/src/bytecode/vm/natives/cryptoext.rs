0d7b21250f7152e6b25cde95e154324a251f125f
richie-rich90454 <rjiang880@gmail.com>
2026-09-13 08:41:07 +0800

    style(native): use is_multiple_of in cryptoext test helper

--- trc/src/bytecode/vm/natives/cryptoext.rs
+++ trc/src/bytecode/vm/natives/cryptoext.rs
@@ -203,7 +203,7 @@ mod cryptoext_native_tests {
     }
 
     fn hex(s: &str) -> Vec<u8> {
-        assert!(s.len() % 2 == 0);
+        assert!(s.len().is_multiple_of(2));
         (0..s.len())
             .step_by(2)
             .map(|i| u8::from_str_radix(&s[i..i + 2], 16).expect("hex"))
